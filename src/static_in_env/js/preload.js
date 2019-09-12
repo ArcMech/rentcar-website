9ca40d3ea1c083a677f9e505017dca725051fd6f
ArcMech <arek.krz@gmail.com>
2019-09-12 20:14:20 +0200

    deleting unnecessary files

--- src/static_in_env/js/preload.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-  const body = document.querySelector("body");
-  const preloader = document.querySelector(".preloader");
-  body.style.overflowY = "visible";
-  preloader.classList.add("finished");
-});
